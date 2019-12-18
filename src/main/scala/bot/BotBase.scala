a4da9bc429ac3b6d9f9205d2fe56ac54919f1a8c
Mack <moskalenkogreg@gmail.com>
2019-12-18 13:57:58 +0500

    ()

--- src/main/scala/bot/BotBase.scala
+++
@@ -1,8 +0,0 @@
-//package bot
-//
-//import cats.effect.{Async, ContextShift}
-//import com.bot4s.telegram.cats.TelegramBot
-//import com.softwaremill.sttp.asynchttpclient.cats.AsyncHttpClientCatsBackend
-//
-//abstract class BotBase[F[_]: Async : ContextShift](val token: String)
-//  extends TelegramBot(token, AsyncHttpClientCatsBackend())
